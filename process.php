7ac755ed668ad96dff57d69e79cb9f7881495257
Dimple Kumari <89607801+Dimple1243@users.noreply.github.com>
2023-02-13 23:11:01 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,25 @@
+ <?php
+ if (isset($_POST["submit"])){
+    $username=$_POST["user"];
+    $password=$_POST["password"];
+    if(!empty($username)&&!empty($password)){
+        $link=mysqli_connect("localhost","root","@Dimple123","login");
+        if($link==false){
+            die(mysqli_connect_error());
+        }
+        $sql="INSERT INTO users(Username,Password) VALUES ('$username','$password')";
+        if(mysqli_query($link,$sql)){
+            echo "Recorded inserted successfully";
+        }
+        else{
+            echo "Something went wrong";
+        }
+
+    }
+    
+    else{
+        
+        echo "Please prove the user details!!!";
+    }
+ }
+ ?>
